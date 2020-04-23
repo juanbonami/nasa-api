1637fab1dda84264bd56de6d17a8b250d3ee4cad
Student_M17 <student_m17@ps-dal-m17.home>
2020-04-23 10:54:19 -0500

    test run to make sure router is working

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+    render() {
+        return (
+            <div>
+                <h1>test run</h1>
+            </div>
+        )
+    }
+}
