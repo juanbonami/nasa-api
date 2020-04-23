fea4466a3f5ef41746d86e4a85ca686ceee5d83a
Student_M17 <student_m17@ps-dal-m17.home>
2020-04-22 23:21:47 -0500

    added constructor and state to class

--- src/components/AstroApi.js
+++ src/components/AstroApi.js
@@ -1,7 +1,16 @@
 import React, { Component } from "react";
 import AstroCard from "./components/AstroCard";
+import axios from "axios";
 
 export default class AstroApi extends Component {
+  constructor(propr) {
+    super(props);
+
+    this.state = {
+      astronomy: [],
+    };
+  }
+
   render() {
     return <div></div>;
   }
