0d0fa90eee4dd37b14f6d4a695572f970ef6a227
Student_M17 <student_m17@ps-dal-m17.home>
2020-04-24 18:42:21 -0500

    cleaning up GalleryApi

--- src/components/GalleryCard.js
+++ src/components/GalleryCard.js
@@ -11,7 +11,7 @@ const GalleryCard = (props) => {
     return (
         <div>
 
-            <img src={arr[0]} alt="" height="200" width="200" ></img>
+            {/* <img src={arr[0]} alt="" height="200" width="200" ></img>
             <img src={arr[1]} alt="" height="200" width="200" ></img>
             <img src={arr[2]} alt="" height="200" width="200" ></img>
             <img src={arr[3]} alt="" height="200" width="200" ></img>
@@ -20,7 +20,13 @@ const GalleryCard = (props) => {
             <img src={arr[6]} alt="" height="200" width="200" ></img>
             <img src={arr[7]} alt="" height="200" width="200" ></img>
             <img src={arr[8]} alt="" height="200" width="200" ></img>
-            <img src={arr[9]} alt="" height="200" width="200" ></img>
+            <img src={arr[9]} alt="" height="200" width="200" ></img> */}
+
+            <form>
+
+                <input type="text" name="search" placeholder="search" />
+                <button>Search</button>
+            </form>
 
         </div>
     )
