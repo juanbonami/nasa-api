f570145416da6b5fb68cdf86ae8dd1260a0758cd
Student_M17 <student_m17@ps-dal-m17.home>
2020-04-24 13:40:35 -0500

    displayed images in dom

--- src/components/GalleryCard.js
+++ src/components/GalleryCard.js
@@ -3,13 +3,24 @@ import React from 'react'
 const GalleryCard = (props) => {
 
     let arr = props.data;
-    console.log(props.data)
+    // console.log(props.data)
+
+
 
 
     return (
         <div>
 
-            <img src={arr} alt="" height="300" width="300" />
+            <img src={arr[0]} alt="" height="200" width="200" ></img>
+            <img src={arr[1]} alt="" height="200" width="200" ></img>
+            <img src={arr[2]} alt="" height="200" width="200" ></img>
+            <img src={arr[3]} alt="" height="200" width="200" ></img>
+            <img src={arr[4]} alt="" height="200" width="200" ></img>
+            <img src={arr[5]} alt="" height="200" width="200" ></img>
+            <img src={arr[6]} alt="" height="200" width="200" ></img>
+            <img src={arr[7]} alt="" height="200" width="200" ></img>
+            <img src={arr[8]} alt="" height="200" width="200" ></img>
+            <img src={arr[9]} alt="" height="200" width="200" ></img>
 
         </div>
     )
