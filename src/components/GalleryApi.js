6ba147bf3da02b8d7deacaef6b2319ac098b6154
Student_M17 <student_m17@ps-dal-m17.home>
2020-04-24 22:49:57 -0500

    added p tag to display description

--- src/components/GalleryApi.js
+++ src/components/GalleryApi.js
@@ -82,6 +82,7 @@ export default class GalleryApi extends Component {
                 <h5> {this.state.title} </h5>
                 <h5> {this.state.date} </h5>
                 <p> {this.state.description} </p>
+                <h5> {this.state.location} </h5>
 
             </div >
 
