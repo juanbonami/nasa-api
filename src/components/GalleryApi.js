0a2918329191a00f55092098882b099e415eeea7
Student_M17 <student_m17@ps-dal-m17.home>
2020-04-24 22:46:10 -0500

    added image from state

--- src/components/GalleryApi.js
+++ src/components/GalleryApi.js
@@ -74,6 +74,7 @@ export default class GalleryApi extends Component {
                 <img src={this.state.gallery[8]} height="200" width="200"></img>
                 <img src={this.state.gallery[9]} height="200" width="200"></img>
                 <img src={this.state.gallery[10]} height="200" width="200"></img>
+                <img src={this.state.gallery[11]} height="200" width="200"></img>
 
 
 
