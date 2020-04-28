2f9babc19b93d366c0ddef6e8d5dc034df4c3e65
Student_M17 <student_m17@ps-dal-m17.home>
2020-04-27 20:06:09 -0500

    deleted images from GalleryCard component, no longer needed

--- src/components/GalleryCard.js
+++ src/components/GalleryCard.js
@@ -5,26 +5,20 @@ const GalleryCard = (props) => {
     let arr = props.data;
     // console.log(props.data)
 
+    handleChange(event) {
+
+    }
+
 
 
 
     return (
         <div>
 
-            {/* <img src={arr[0]} alt="" height="200" width="200" ></img>
-            <img src={arr[1]} alt="" height="200" width="200" ></img>
-            <img src={arr[2]} alt="" height="200" width="200" ></img>
-            <img src={arr[3]} alt="" height="200" width="200" ></img>
-            <img src={arr[4]} alt="" height="200" width="200" ></img>
-            <img src={arr[5]} alt="" height="200" width="200" ></img>
-            <img src={arr[6]} alt="" height="200" width="200" ></img>
-            <img src={arr[7]} alt="" height="200" width="200" ></img>
-            <img src={arr[8]} alt="" height="200" width="200" ></img>
-            <img src={arr[9]} alt="" height="200" width="200" ></img> */}
 
             <form>
 
-                <input type="text" name="search" placeholder="search" />
+                <input type="text" name="search" placeholder="search" onChange={this.handleChange} />
                 <button>Search</button>
             </form>
 
