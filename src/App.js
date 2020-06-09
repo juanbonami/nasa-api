bc5e6a8e85126582c5241ebe5ab343e65dbf2edd
Student_M17 <student_m17@ps-dal-m17.home>
2020-06-09 08:42:42 -0500

    switched react-router setup from App.js to Navbar.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,11 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
 import "./App.css";
-import AstroApi from "./components/AstroApi";
-import About from "./components/About";
-import GalleryApi from "./components/GalleryApi";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-
-      <Router>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/gallery">Gallery</Link>
-          <Link to="/about">About</Link>
-        </nav>
-
-        <Switch>
-          <Route path="/gallery" component={GalleryApi} />
-          <Route path="/about" component={About} />
-          <Route path="/" component={AstroApi} />
-        </Switch>
-      </Router>
-      {/* <GalleryApi /> */}
+      <Navbar />
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import AstroApi from './AstroApi';
+import About from './About';
+import GalleryApi from './GalleryApi';
 
 export default class Navbar extends Component {
     render() {
         return (
             <div>
+                <Router>
+                    <nav>
+                        <Link to="/">Home</Link>
+                        <Link to="/gallery">Gallery</Link>
+                        <Link to="/about">About</Link>
+                    </nav>
 
+                    <Switch>
+                        <Route path="/gallery" component={GalleryApi} />
+                        <Route path="/about" component={About} />
+                        <Route path="/" component={AstroApi} />
+                    </Switch>
+                </Router>
             </div>
         )
     }
