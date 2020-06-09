dff6eba4724406e51abef18a8a2f025ca02250fe
Student_M17 <student_m17@ps-dal-m17.home>
2020-06-09 09:22:39 -0500

    experimenting with Menu component

--- src/components/GalleryCard.js
+++ src/components/GalleryCard.js
@@ -1,27 +1,58 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Menu, Segment } from 'semantic-ui-react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import AstroApi from './AstroApi';
+import About from './About';
+import GalleryApi from './GalleryApi';
 
-const GalleryCard = (props) => {
+export default class GalleryCard extends Component {
+    state = { activeItem: 'home' }
 
-    let arr = props.data;
-    // console.log(props.data)
+    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
 
+    render() {
+        const { activeItem } = this.state
 
+        return (
+            <Segment inverted>
+                <Menu inverted pointing secondary>
+                    <Menu.Item
+                        name='Home'
+                        active={activeItem === 'Home'}
+                        onClick={this.handleItemClick}
 
+                    />
+                    <Menu.Item
+                        name='Gallery'
+                        active={activeItem === 'Gallery'}
+                        onClick={this.handleItemClick}
+                    />
+                    <Menu.Item
+                        name='About'
+                        active={activeItem === 'About'}
+                        onClick={this.handleItemClick}
+                    />
 
+                    <Router>
+                        <nav>
+                            <Link to="/">Home</Link>
+                            <Link to="/gallery">Gallery</Link>
+                            <Link to="/about">About</Link>
+                        </nav>
 
-
-    return (
-        <div>
-
-
-            {/* <form>
-
-                <input type="text" name="search" placeholder="search" onChange={this.handleChange} />
-                <button>Search</button>
-            </form> */}
-
-        </div>
-    )
+                        <Switch>
+                            <Route path="/gallery" component={GalleryApi} />
+                            <Route path="/about" component={About} />
+                            <Route path="/" component={AstroApi} />
+                        </Switch>
+                    </Router>
+                </Menu>
+            </Segment>
+        )
+    }
 }
-
-export default GalleryCard;
