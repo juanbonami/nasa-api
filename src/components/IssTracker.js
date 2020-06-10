921091f739d08aedf57e9cb82b3e06e2bfacf436
Student_M17 <student_m17@ps-dal-m17.home>
2020-06-10 01:54:58 -0500

    created new component for new api that tracks the position of the iss

---
+++ src/components/IssTracker.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+
+export default class IssTracker extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            latitude: '',
+            longitude: ''
+        }
+    }
+
+
+
+
+
+
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
