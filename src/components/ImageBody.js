f1ca395c05ea6e340393e98f476a5c344b740a5a
Juan Bonami <Juanbonami@gmail.com>
2020-06-11 16:42:50 -0500

    added new component for image body

---
+++ src/components/ImageBody.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class ImageBody extends Component {
+    render() {
+        return (
+            <div className="space-giphy">
+                    <h1> hello </h1>
+                    <h1> hello </h1>
+            </div>
+        )
+    }
+}
