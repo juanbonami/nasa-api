4ef1fede4a1c1b1917b232ac5283830a353d0fa1
Juan Bonami <Juanbonami@gmail.com>
2020-06-11 18:30:26 -0500

    added text to image body component with some breaks

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,6 +10,7 @@ export default class Footer extends Component {
                         <div className="col-md-3 col-sm-6">
                             <h4>Contact</h4>
                             <ul className="list-unstyled">
+                                <br/>
                                 <li> <Icon circular inverted name='mail' /> juanbonami@gmail.com</li>                              
                                 <li> <Icon circular inverted name='github' /> <a href="https://github.com/juanbonami"> Github </a></li>
                                 <li> <Icon circular inverted name='linkedin' /> <a href="https://www.linkedin.com/in/juan-bonami-261909128/">LinkedIn</a></li>
@@ -19,10 +20,9 @@ export default class Footer extends Component {
                         <div className="col-md-3 col-sm-6">
                             <h4>Services</h4>
                             <ul className="list-unstyled">
+                                <br/>
                                 <li><a href="https://api.nasa.gov/">NASA Astronomy Picture of The Day</a></li>
                                 <li><a href="http://open-notify.org/Open-Notify-API/ISS-Location-Now/">ISS Tracker</a></li>
-                                <li>Lorem ipsum</li>
-                                <li>Lorem ipsum</li>
                             </ul>
                         </div>
 
--- src/components/ImageBody.js
+++ src/components/ImageBody.js
@@ -4,8 +4,7 @@ export default class ImageBody extends Component {
     render() {
         return (
             <div className="space-giphy">
-                    <h1> </h1>
-                    <h1>  </h1>
+                    <p style={{padding: 20}}> "There must be no barriers to freedom of inquiry. There is no place for dogma in science. The scientist is free, and must be free to ask any question, to doubt any assertion, to seek for any evidence, to correct any errors... <br/>  Our political life is also predicated on openness. We know that the only way to avoid error is to detect it and that the only way to detect it is to be free to inquire.<br/> And we know that as long as men are free to ask what they must, free to say what they think, free to think what they will, freedom can never be lost,<br/> and science can never regress."<br/> <br/> -J. Robert Oppenheimer </p>
             </div>
         )
     }
