584ae702d93b642c5a70e7e2f380c63ff6717fd1
Juan Bonami <Juanbonami@gmail.com>
2020-06-11 03:36:48 -0500

    added divs with classname one for main footer and the other for container

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <div className="main-footer">
+                <div className="container-footer">
+                    
+                    {/* ****** */}
+                    <br />
+                        
+                        
+                </div>
+            </div>
+        )
+    }
+}
