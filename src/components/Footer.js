62872724105168e744b4ca97e453f3136bcdb7f5
Juan Bonami <Juanbonami@gmail.com>
2020-06-11 03:41:43 -0500

    added column div and an un ordered list

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,15 @@ export default class Footer extends Component {
             <div className="main-footer">
                 <div className="container-footer">
                     <div className="row">
-                        
+                        <div className="col-md-3 col-sm-6">
+                            <h4>Lorem ipsum</h4>
+                            <ul className="list-unstyled">
+                                <li>Lorem ipsum</li>
+                                <li>Lorem ipsum</li>
+                                <li>Lorem ipsum</li>
+                                <li>Lorem ipsum</li>
+                            </ul>
+                        </div>
 
                         
 
