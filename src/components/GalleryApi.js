24e1fff46e5a075a951a95130ca8e6ad742b3f9c
Juan Bonami <Juanbonami@gmail.com>
2020-06-11 19:50:43 -0500

    added grid column to and divider to divide the search bar and the search button

--- src/components/GalleryApi.js
+++ src/components/GalleryApi.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import { Segment, Input } from 'semantic-ui-react'
 import { Button } from 'semantic-ui-react'
-import { Grid, Image } from 'semantic-ui-react'
+import { Grid, Image, Divider } from 'semantic-ui-react'
+import { Icon } from 'semantic-ui-react'
 import logo from '../giphy/giphy.gif'
 
 
@@ -56,9 +57,23 @@ export default class GalleryApi extends Component {
               
                 <form onSubmit={this.submitHandler} >
 
-                    <h1> Search the NASA Image Library </h1>
-                    <Input type="text" name="name" value={this.userInput} onChange={this.changeHandler} inverted placeholder='Search...' />
-                    <Button type="submit" primary>Search</Button>
+                <Segment placeholder inverted>
+                    <Grid columns={2} relaxed='very' stackable>
+                        <Grid.Column>
+                            <h1> Search the NASA Image Library </h1>
+                            <Input type="text" name="name" value={this.userInput} onChange={this.changeHandler} inverted placeholder='Search...' />
+                            
+                        </Grid.Column>
+
+                        <Grid.Column verticalAlign='middle'>
+                            <Button type="submit" primary>Search</Button>
+                        </Grid.Column>
+                    </Grid>
+
+                    <Divider vertical inverted> <Icon circular inverted name='search' /> </Divider>
+                </Segment>
+
+                    
                     
                 </form>
 
