e1eafe5e4f84e0fdeb7ff3ae87cc3e8eece9000f
Juan Bonami <Juanbonami@gmail.com>
2021-03-24 22:34:22 -0500

    cleaned up some code to redo layout

--- src/components/AstroCard.js
+++ src/components/AstroCard.js
@@ -36,22 +36,11 @@ const AstroCard = (props) => {
     
     return (
       <div className="astro-color">   
-  
         <h3>Astronomy Photo of The Day</h3>
-        <h6 style={{paddingTop: 20}}>{title}</h6>  
-  
-        <Grid style={{
-          paddingLeft: 130
-        }}>
-          <Grid.Column width={5}>
-            
-            <Image src={hdurl} alt={title} />
-            <span>{date} {copyright}</span>
-          </Grid.Column>
-          <Grid.Column width={9} style={{paddingTop: 70}}>
-            <p>{explanation}</p>
-          </Grid.Column>
-        </Grid>
+        <h6>{title}</h6>  
+        <Image src={hdurl} alt={title} />
+        <span>{date} {copyright}</span>
+        <p>{explanation}</p>
       </div>
       
     )
